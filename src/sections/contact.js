47697e69d1ee52faf768005f918b0eaa21be2e58
Chelsea Crowson <chelsea.crowson@me.com>
2018-12-10 13:52:58 -0600

    phone number on form

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -16,16 +16,24 @@ export default class Contact extends Component {
 
 
 				<form name="contact" method="post">
-					<input type="hidden" name="form-name" value="contact" />
-					<p>
-						<label>Your Name: <input type="text" name="name" /></label>
+					<input type="hidden" name="form-name" value="contact" required />
+					{/* <p>
+						<label>Your Name: <input type="text" name="name" required /></label>
 					</p>
 					<p>
-						<label>Your Email: <input type="email" name="email" /></label>
+						<label>Your Email: <input type="email" name="email" required /></label>
 					</p>
 					<p>
-						<label>Message: <textarea name="message"></textarea></label>
+						<label>Phone Number: <input type="tel" name="telephone" required /></label>
 					</p>
+					<p>
+						<textarea name="message" ></textarea>
+					</p> */}
+					<input type="text" name="name" placeholder="Name" required />
+					<input type="email" name="email" placeholder="Email" required />
+					<input type="tel" name="phone" placeholder="Phone Number" required />
+					<textarea rows="5" cols="50" name="message" placeholder="Message..."
+						required></textarea>
 					<p>
 						<button type="submit">Send</button>
 					</p>
